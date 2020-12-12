32126af3d1f0fe94259fb51389fb6c701e9351de
rougga <rouggama@gmail.com>
2020-12-12 13:43:56 +0100

    Signed-off-by: ROUGGA <rouggama@gmail.com> Signed-off-by:rougga <rouggama@gmail.com>

--- src/java/main/servlet/Login.java
+++ src/java/main/servlet/Login.java
@@ -24,7 +24,10 @@ public class Login extends HttpServlet {
             String window = request.getParameter("window");
             if(true){
                 
+            }else{
+                
             }
+            
             
             
         }
